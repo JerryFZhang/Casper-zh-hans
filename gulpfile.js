047b5f90f0f5d80d860c9696449bb7dd47d14d28
Jerry F. Zhang <jerry.fengwei@gmail.com>
2020-11-14 11:17:08 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,27 @@
-const {series, watch, src, dest} = require('gulp');
+const {series, watch, src, dest, parallel} = require('gulp');
 const pump = require('pump');
+const path = require('path');
+const releaseUtils = require('@tryghost/release-utils');
+const inquirer = require('inquirer');
 
 // gulp plugins and utils
-var livereload = require('gulp-livereload');
-var postcss = require('gulp-postcss');
-var zip = require('gulp-zip');
-var uglify = require('gulp-uglify');
-var beeper = require('beeper');
+const livereload = require('gulp-livereload');
+const postcss = require('gulp-postcss');
+const zip = require('gulp-zip');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const beeper = require('beeper');
+const fs = require('fs');
 
 // postcss plugins
-var autoprefixer = require('autoprefixer');
-var colorFunction = require('postcss-color-function');
-var cssnano = require('cssnano');
-var customProperties = require('postcss-custom-properties');
-var easyimport = require('postcss-easy-import');
+const autoprefixer = require('autoprefixer');
+const colorFunction = require('postcss-color-mod-function');
+const cssnano = require('cssnano');
+const easyimport = require('postcss-easy-import');
+
+const REPO = 'TryGhost/Casper';
+const REPO_READONLY = 'TryGhost/Casper';
+const CHANGELOG_PATH = path.join(process.cwd(), '.', 'changelog.md');
 
 function serve(done) {
     livereload.listen();
@@ -29,18 +37,22 @@ const handleError = (done) => {
     };
 };
 
-function css(done) {
-    var processors = [
-        easyimport,
-        customProperties({preserve: false}),
-        colorFunction(),
-        autoprefixer({browsers: ['last 2 versions']}),
-        cssnano()
-    ];
+function hbs(done) {
+    pump([
+        src(['*.hbs', 'partials/**/*.hbs']),
+        livereload()
+    ], handleError(done));
+}
 
+function css(done) {
     pump([
         src('assets/css/*.css', {sourcemaps: true}),
-        postcss(processors),
+        postcss([
+            easyimport,
+            colorFunction(),
+            autoprefixer(),
+            cssnano()
+        ]),
         dest('assets/built/', {sourcemaps: '.'}),
         livereload()
     ], handleError(done));
@@ -48,7 +60,12 @@ function css(done) {
 
 function js(done) {
     pump([
-        src('assets/js/*.js', {sourcemaps: true}),
+        src([
+            // pull in lib files first so our own code can depend on it
+            'assets/js/lib/*.js',
+            'assets/js/*.js'
+        ], {sourcemaps: true}),
+        concat('casper.js'),
         uglify(),
         dest('assets/built/', {sourcemaps: '.'}),
         livereload()
@@ -56,25 +73,115 @@ function js(done) {
 }
 
 function zipper(done) {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
+    const filename = require('./package.json').name + '.zip';
 
     pump([
         src([
             '**',
             '!node_modules', '!node_modules/**',
-            '!dist', '!dist/**'
+            '!dist', '!dist/**',
+            '!yarn-error.log'
         ]),
         zip(filename),
-        dest(targetDir)
+        dest('dist/')
     ], handleError(done));
 }
 
-const watcher = () => watch('assets/css/**', css);
+const cssWatcher = () => watch('assets/css/**', css);
+const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
+const watcher = parallel(cssWatcher, hbsWatcher);
 const build = series(css, js);
-const dev = series(build, serve, watcher);
 
 exports.build = build;
 exports.zip = series(build, zipper);
-exports.default = dev;
+exports.default = series(build, serve, watcher);
+
+exports.release = () => {
+    // @NOTE: https://yarnpkg.com/lang/en/docs/cli/version/
+    // require(./package.json) can run into caching issues, this re-reads from file everytime on release
+    var packageJSON = JSON.parse(fs.readFileSync('./package.json'));
+    const newVersion = packageJSON.version;
+
+    if (!newVersion || newVersion === '') {
+        console.log(`Invalid version: ${newVersion}`);
+        return;
+    }
+
+    console.log(`\nCreating release for ${newVersion}...`);
+
+    let config;
+    try {
+        config = require('./config');
+    } catch (err) {
+        config = null;
+    }
+
+    if (!config || !config.github || !config.github.token) {
+        console.log('Please copy config.example.json and configure Github token.');
+        return;
+    }
+
+    let compatibleWithGhost;
+
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'compatibleWithGhost',
+        message: 'Which version of Ghost is it compatible with?',
+        default: '3.0.0'
+    }])
+    .then(result => {
+        compatibleWithGhost = result.compatibleWithGhost;
+        return Promise.resolve();
+    })
+    .then(() => releaseUtils.releases.get({
+        userAgent: 'Casper',
+        uri: `https://api.github.com/repos/${REPO_READONLY}/releases`
+    }))
+    .then((response) => {
+        if (!response || !response.length) {
+            console.log('No releases found. Skipping...');
+            return;
+        }
+
+        let previousVersion = response[0].tag_name || response[0].name;
+        console.log(`Previous version: ${previousVersion}`);
+        return Promise.resolve(previousVersion);
+    })
+    .then((previousVersion) => {
+        const changelog = new releaseUtils.Changelog({
+            changelogPath: CHANGELOG_PATH,
+            folder: path.join(process.cwd(), '.')
+        });
+
+        changelog
+            .write({
+                githubRepoPath: `https://github.com/${REPO}`,
+                lastVersion: previousVersion
+            })
+            .sort()
+            .clean();
+
+        return Promise.resolve();
+    })
+    .then(() => releaseUtils.releases.create({
+        draft: true,
+        preRelease: false,
+        tagName: newVersion,
+        releaseName: newVersion,
+        userAgent: 'Casper',
+        uri: `https://api.github.com/repos/${REPO}/releases`,
+        github: {
+            token: config.github.token
+        },
+        content: [`**Compatible with Ghost ≥ ${compatibleWithGhost}**\n\n`],
+        changelogPath: CHANGELOG_PATH
+    }))
+    .then((response) => {
+        console.log(`\nRelease draft generated: ${response.releaseUrl}\n`);
+        return Promise.resolve();
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
+};
